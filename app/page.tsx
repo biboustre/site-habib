e7dd060ba1aa69f1a4b642398bc197560b2761b5
MAAMAR BENHADJAR <biboustre@hotmail.com>
2024-10-04 22:22:06 +0200

    maj

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,6 @@ import { portfolioItems } from "./data/data";
 import ContactCard from "./components/molecules/contact/ContactCard";
 
 export default function Home() {
-  // const [open, setOpen] = useState(false);
-
-  // const handlClick = () => {
-  //   setOpen(!open);
-  // };
-
   return (
     <>
       <BannerMain
@@ -33,7 +27,6 @@ export default function Home() {
         <h2 className="text-center text-4xl font-bold text-white md:text-6xl">
           Ce que nous proposons
         </h2>
-        {/* <hr className="w-64 m-auto" /> */}
         <section className="flex flex-col gap-20 px-5 pb-20 text-justify text-lg font-semibold text-white lg:flex-row lg:gap-5 xl:px-20">
           {descriptions.map((description) => (
             <ServiceDescription
@@ -56,7 +49,6 @@ export default function Home() {
           height={500}
           width={500}
           objectFit="cover"
-          // layout="fill"
         />
       </section>
       <section
@@ -85,7 +77,6 @@ export default function Home() {
           Voir la stack technique
         </Link>
       </div>
-
       <ContactCard />
     </>
   );
