478b119782e3cf2bd448ba3612cc0ece26c99b1e
MAAMAR BENHADJAR <biboustre@hotmail.com>
2024-10-25 18:01:07 +0200

    MAJ img banner

--- app/data/cardRecruteurData.js
+++ app/data/cardRecruteurData.js
@@ -1,5 +1,5 @@
 export const cardRecruteur = [
-    { id: 1, value: "3", label: "Années d'expérience" },
+    { id: 1, value: "4", label: "Années d'expérience" },
     { id: 2, value: "12", label: "Projets d'école" },
     { id: 3, value: "6", label: "Projets Pro" },
     { id: 4, value: "999", label: "% de motivation" },
