98a8655ee7795abad38df535eeed55bc03e8da86
Habib <biboustre@hotmail.com>
2025-04-27 03:28:24 +0200

    maj

--- app/components/organism/serviceGraphique/ServicesGraphique.tsx
+++ app/components/organism/serviceGraphique/ServicesGraphique.tsx
@@ -21,33 +21,37 @@ const services = [
 
 function ServicesGraphique() {
   return (
-    <section className="flex h-auto flex-col items-center justify-center gap-10 bg-neutral-950 px-2 py-32 text-white md:px-8 lg:flex-row lg:items-stretch">
+    <section className="flex h-auto flex-col items-center justify-center gap-14 bg-neutral-950 px-2 py-32 text-white sm:px-3 md:px-0 lg:flex-row lg:items-stretch">
       {/* Conteneur gauche (Texte + Cartes) */}
-      <section className="flex flex-col gap-10 lg:w-2/3">
+      <section className="flex flex-col gap-24 lg:w-2/3">
         {/* Texte de la section */}
-        <section className="flex items-center justify-center rounded-lg bg-gradient-to-r from-orange-800 to-orange-400 p-10">
-          <h1 className="text-center text-2xl font-bold lg:text-start lg:text-3xl">
+        <section className="btn-gradient flex cursor-default items-center justify-center rounded-lg shadow-2xl shadow-yellow-900  transition duration-300 hover:scale-95 md:justify-start ">
+          <h1 className="colorPrimaryStack max-w-[1200px] p-10 text-center text-2xl font-bold hover:text-white lg:text-start xl:text-3xl 2xl:py-16">
             Nous proposons une gamme de services de design graphique. Que ce
             soit pour la création de flyers, logos, ou de cartes de restaurant.
           </h1>
         </section>
 
         {/* Cartes de services */}
-        <section className="flex w-full flex-wrap justify-center gap-5 md:flex-nowrap md:justify-normal md:gap-8">
+        <section className="flex w-full flex-wrap justify-center gap-14 md:flex-nowrap md:justify-normal md:gap-5 xl:gap-14 ">
           {services.map((service, index) => (
             <section
               key={index}
-              className="flex max-h-[350px] w-full max-w-[500px] basis-full flex-col  gap-5 overflow-hidden rounded-3xl bg-zinc-800 p-5 shadow-lg md:basis-1/3 lg:size-[20vw]"
+              className="btn-gradient flex h-[250px] max-h-[350px] w-full max-w-[500px] basis-full  cursor-default flex-col gap-10 overflow-hidden rounded-3xl bg-neutral-900/30   p-5 shadow-lg  shadow-yellow-900 duration-300 hover:scale-95 md:min-h-[350px] md:basis-1/3 lg:size-[20vw]"
             >
-              <h2 className="text-lg font-bold">{service.title}</h2>
-              <p className="text-sm">{service.description}</p>
+              <h2 className="colorPrimaryStack text-xl font-bold md:text-2xl">
+                {service.title}
+              </h2>
+              <p className="text- md:text-base xl:text-lg">
+                {service.description}
+              </p>
             </section>
           ))}
         </section>
       </section>
 
       {/* Image à droite */}
-      <figure className="h-auto w-full overflow-hidden rounded-lg lg:w-1/5">
+      <figure className="h-auto w-full cursor-default overflow-hidden rounded-lg shadow-lg shadow-slate-700 transition duration-300 hover:scale-105 lg:w-1/5">
         <Image
           src="/images/photo_ordinateur_1.webp"
           alt="Graphique"
