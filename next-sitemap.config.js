69e9153966b787ababd4bb513adfca0f0298851d
Habib <biboustre@hotmail.com>
2025-07-19 13:45:02 +0200

    creation sitemap.xml etc

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://web-ibou.vercel.app', // Mets ici ton vrai domaine si tu en as un
+  generateRobotsTxt: true,
+}
